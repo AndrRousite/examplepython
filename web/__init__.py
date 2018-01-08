061e97307d3038bb20dd03bfb42631e95ab8d5b4
liu-feng <w710989327@foxmail.com>
2018-01-08 18:03:00 +0800

    dsd dd d

---
+++ web/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# @author liu-feng
+# @email w710989327@foxmail.com
+# @time 2018/1/8 17:15
+
+__author__ = 'liu-feng'
+__email__ = 'w710989327@foxmail.com'
+
---
+++ web/app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+
+# @author liu-feng
+# @email w710989327@foxmail.com
+# @time 2018/1/8 17:31
+from flask import Flask, request
+
+__author__ = 'liu-feng'
+__email__ = 'w710989327@foxmail.com'
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return '<h1>Home</h1>'
+
+
+@app.route('/article', methods=['GET'])
+def article():
+    return '''<form action="/submit" method="post">
+              <p><input name="username"></p>
+              <p><input name="password" type="password"></p>
+              <p><button type="submit">Sign In</button></p>
+              </form>'''
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    # 需要从request对象读取表单内容：
+    if request.form['username'] == 'admin' and request.form['password'] == 'password':
+        return '<h3>Hello, admin!</h3>'
+    return '<h3>Bad username or password.</h3>'
+
+
+if __name__ == '__main__':
+    app.run()
